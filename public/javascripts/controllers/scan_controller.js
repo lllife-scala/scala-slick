5079e11a0e1b2941736d6fa1b8a9d7a81af06fa5
Somnuk Wongkhan <somnuk.wk@gmail.com>
2014-02-16 23:10:10 +0700

    update dashboard animation

--- public/javascripts/controllers/scan_controller.js
+++ public/javascripts/controllers/scan_controller.js
@@ -1,12 +1,29 @@
-app.controller("scanController", function($scope, $log, scanService){
+app.controller("scanController", function($scope, $log, scanService, $timeout){
 
     $scope.scans = [];
 
     // listen socket message
     // pass json object as parameter...
     function handler(info) {
+
+        // copy page value
+        var page = info.data.page;
+
+        // reset to zero
+        info.data.page = 0;
+
+        // push info with zero page
         $scope.scans.push(info);
+
+        // applay change
         $scope.$apply();
+
+        $timeout(function(){
+            info.data.page = page;
+            $log.info("timeout >> " + new Date());
+        }, 1000);
+
+        $log.info("handler >> " + new Date());
     }
 
     // when document ready..
